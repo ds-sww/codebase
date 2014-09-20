bd20f7228ec12715dd9594fb45b653a661ef33e2
whimsycwd <whimsy@Chenweidongs-MacBook-Pro.local>
2014-09-20 20:47:40 +0800

    commit HelloWorld Demo

---
+++ 11300240057/assignment0/HelloWorld.cpp
@@ -0,0 +1,18 @@
+
+#include<iostream>
+#include<cstdio>
+
+using namespace std;
+
+/**
+ *  @auhtor whimsy
+ *  @date   2014.9.20
+ *  Hello World Demo
+ *
+ */
+
+int main() {
+    cout << "Hello World!" << endl;
+    return 0;
+}
+
