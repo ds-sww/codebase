f3b8c49e7eb002e0fda5b2da114325f67b6b9839
yangyue <this.isssssyy@gmail.com>
2014-09-28 20:54:14 +0800

    fully finish assignment 1

--- 13307130265/assignment1/src/ArrayList.cpp
+++ 13307130265/assignment1/src/ArrayList.cpp
@@ -5,6 +5,8 @@
  * 类似Vector
  */
 #include <iostream>
+#include <cstring>
+#include <stdexcept>
 #include <cassert>
 #include "List.h"
 
@@ -17,6 +19,7 @@ class ArrayList : public List<T> {
 		int list_size;
 		int capicity;
 		T *array;
+		int revision;
 		const static int init_capicity = 32;
 		
 		void range_check(const int &index) const {
@@ -45,15 +48,20 @@ class ArrayList : public List<T> {
 			array = new_array;
 		}
 	public:
+		Iterator<T> *iterator() {
+			return new ArrayListIterator<T> (this, array, revision);
+		}
 		ArrayList(){
 			list_size = 0;
 			capicity = init_capicity;
 			array = new T[capicity];
+			revision = 0;
 		}
 		ArrayList(const int &size) : list_size(size) {
 			capicity = max(init_capicity, (1<<(32-__builtin_clz(size))));
 			array = new T[capicity];
 			memset(array, 0, sizeof(array[0])*list_size);
+			revision = 0;
 		}
 		int size() const {
 			return list_size;
@@ -62,6 +70,8 @@ class ArrayList : public List<T> {
 			return list_size == 0;
 		}
 		void add(const T &element) {
+			++revision;
+			
 			expandMemory(list_size+1);
 			array[list_size++] = element;
 		}
@@ -70,6 +80,8 @@ class ArrayList : public List<T> {
 			return array[index];
 		}
 		T remove(const int &index) {
+			++revision;
+			
 			range_check(index);
 			T res = array[index];
 			for (int x = index; x+1 < list_size; ++x) array[x] = array[x+1];
@@ -79,4 +91,26 @@ class ArrayList : public List<T> {
 		~ArrayList() {
 			delete [] array;
 		}
+		
+		template<typename E>
+		class ArrayListIterator : public Iterator<E> {
+			private:
+				ArrayList *owner;
+				T *cur;
+				int revision;
+			public:
+				ArrayListIterator(){}
+				ArrayListIterator(ArrayList<T> *owner, T *cur, int revision) : owner(owner), cur(cur), revision(revision) {}
+				bool hasNext() {
+//					assert(revision == owner->revision);
+					if (revision != owner->revision) {
+						throw logic_error("the ArrayList HAS been changed");
+					}
+					return (cur != (owner->array+owner->list_size-1));
+				}
+				T next() {
+					return *cur++; 
+				}
+		};
 };
+
