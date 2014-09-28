82239ccc0895b9263b12c34674b9c35d36d01d97
CodingYue <this.isssssyy@gmail.com>
2014-09-28 19:25:56 +0800

    ...

--- 13307130265/assignment1/ArrayListBase/List.h
+++ 13307130265/assignment1/ArrayListBase/List.h
@@ -11,9 +11,9 @@ class List {
     public :
         virtual int size() const = 0;
         virtual bool isEmpty() const = 0;
-        virtual T get(int index) const = 0;
-        virtual void add(T element) = 0;
-        virtual T remove(int index) = 0;
+        virtual T get(const int &index) const = 0;
+        virtual void add(const T &element) = 0;
+        virtual T remove(const int &index) = 0;
 
 };
 
--- 13307130265/assignment1/ArrayListWithIterator-only-header/ArrayList.cpp
+++ 13307130265/assignment1/ArrayListWithIterator-only-header/ArrayList.cpp
@@ -16,15 +16,16 @@ class ArrayList : public List<T> {
 	private:
 		int list_size;
 		int capicity;
+		int revision;
 		T *array;
 		const static int init_capicity = 32;
 		
-		void range_check(const int &index) const {
+		void range_check(int index) const {
 			assert(index >= 0 && index < list_size);
 		}
 		// WTF vector implements so slow !?
 		
-		void expandMemory(const int &size) {
+		void expandMemory(int size) {
 			if (size <= capicity) return;
 			
 			capicity *= 2;
@@ -34,7 +35,7 @@ class ArrayList : public List<T> {
 			array = new_array;
 		}
 		
-		void shrinkMemory(const int &size) {
+		void shrinkMemory(int size) {
 			if (size >= capicity/2) return;
 			if (capicity == init_capicity) return;
 			
@@ -45,15 +46,20 @@ class ArrayList : public List<T> {
 			array = new_array;
 		}
 	public:
+		Iterator<T> *iterator() {
+			return &Iterator(this, array, revision);
+		}
 		ArrayList(){
 			list_size = 0;
 			capicity = init_capicity;
 			array = new T[capicity];
+			revision = 0;
 		}
-		ArrayList(const int &size) : list_size(size) {
+		ArrayList(int size) : list_size(size) {
 			capicity = max(init_capicity, (1<<(32-__builtin_clz(size))));
 			array = new T[capicity];
 			memset(array, 0, sizeof(array[0])*list_size);
+			revision = 0;
 		}
 		int size() const {
 			return list_size;
@@ -61,15 +67,15 @@ class ArrayList : public List<T> {
 		bool isEmpty() const {
 			return list_size == 0;
 		}
-		void add(const T &element) {
+		void add(T element) {
 			expandMemory(list_size+1);
 			array[list_size++] = element;
 		}
-		T get(const int &index) const {
+		T get(int index) const {
 			range_check(index);
 			return array[index];
 		}
-		T remove(const int &index) {
+		T remove(int index) {
 			range_check(index);
 			T res = array[index];
 			for (int x = index; x+1 < list_size; ++x) array[x] = array[x+1];
@@ -80,3 +86,20 @@ class ArrayList : public List<T> {
 			delete [] array;
 		}
 };
+template<typename T>
+class Iterator : public Iterator<T> {
+	private:
+		ArrayList *owner;
+		T *cur;
+		int revision;
+	public:
+		Iterator(ArrayList *owner, T *cur, int revision) : owner(owner), cur(cur), revision(revision) {}
+		bool hasNext() {
+			assert(revision == owner->revision);
+			return (cur != (onwer->array+onwer->list_size-1));
+		}
+		T next() {
+			return *cur++; 
+		}
+};
+
