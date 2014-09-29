981676fcc4ae1d57ccb68e7bef00f4b393665f77
tarmo <tarmo@tarmo.org>
2014-09-29 13:23:15 +0800

    0929

--- 13307130098/assignment1/src/ArrayListBase-only-header/ArrayListTest.cpp
+++ 13307130098/assignment1/src/ArrayListBase-only-header/ArrayListTest.cpp
@@ -1,4 +1,4 @@
-/**
+﻿/**
  * @author whimsycwd
  * @date   2014.9.26
  * 只测试了Type = int 的情况
@@ -17,8 +17,10 @@
 using namespace std;
 
 
-void insert_million_entry(List<int>& list) {
+void insert_million_entry() {
     cout << "insert_million_entry start" << endl;
+
+    ArrayList<int> list;
 
     for (int i = 0; i< 1000000; ++i) {
         list.add(i); 
@@ -32,9 +34,10 @@ void insert_million_entry(List<int>& list) {
     
 }
 
-void delete_after_insert_entrys(List<int>& list) {
+void delete_after_insert_entrys() {
     cout << "delete_after_insert_entrys start" << endl;
 
+    ArrayList<int> list;
     for (int i = 0; i< 1000; ++i) {
         list.add(i);
     }
@@ -68,11 +71,12 @@ void erase(vector<int> & vec, int index) {
     vec.erase(vec.begin() + index);
 }
 
-void random_insert_delete_entrys(List<int>& list, double insertFactor) {
+void random_insert_delete_entrys(double insertFactor) {
     cout << "random_insert_delete_entrys start   insertFactor : " << insertFactor <<  endl;
 
     srand((unsigned long) time(NULL));    
 
+    ArrayList<int> list;
     vector<int> vec;  
 
 
@@ -103,62 +107,15 @@ void random_insert_delete_entrys(List<int>& list, double insertFactor) {
     cout << "random_insert_delete_entrys end." << endl;
 }
 
-void iterate_through_list(List<int>& list) {
-    cout << "iterate_through_list start" << endl;
-
-    for (int i = 0; i < 10; ++i) {
-        list.add(i);
-    }
-
-    Iterator<int>* iter = list.iterator();
-    
-    int cnt = 0;
-    while (iter->hasNext()) {
-        assert(list.get(cnt++) == iter->next());
-    }
-
-    delete iter;
-    cout << "iterate_through_list end" << endl;
-
-}
-
-void iterator_fast_fail_feature(List<int>& list) {
-    cout << "iterator_fast_fail_feature start" << endl;
-
-    list.add(1);
-    Iterator<int>* iter = list.iterator();
-    try {    
-        while (iter->hasNext()) {
-            iter->next();
-            list.add(2);
-        }
-    } catch (logic_error e) {
-        cout << e.what() << endl;
-    }
-
-    delete iter;
-
-    cout << "iterator_fast_fail_feature end" << endl;
-}
 
 int main() {
-   
-    ArrayList<int> list;
-    insert_million_entry(list);
     
-    ArrayList<int> list2;
-    delete_after_insert_entrys(list2);
+    insert_million_entry();
+    delete_after_insert_entrys();
     
     for (int i = 0; i< 10; ++i) {
-        ArrayList<int> list3;
-        random_insert_delete_entrys(list3, i);
+        random_insert_delete_entrys(i);
     }
-
-    ArrayList<int> list4;
-    iterate_through_list(list4);
-
-    ArrayList<int> list5;
-    iterator_fast_fail_feature(list5);
     
     return 0;
 }
--- 13307130098/assignment1/src/ArrayListWithIterator-only-header/ArrayList.cpp
+++ 13307130098/assignment1/src/ArrayListWithIterator-only-header/ArrayList.cpp
@@ -4,12 +4,167 @@
  * @date   2014.9.26
  * 类似Vector
  */
-#include <iostream>
+#include <cstring>
 #include <cassert>
+#include <stdexcept>
 #include "List.h"
 
 using namespace std;
 
 template <typename T>
 class ArrayList : public List<T> {
+private:
+	int version;
+	T *link[30];
+	int sz;
+	int pow2[30];
+	
+	int getPos(int index) const
+	{
+		assert(0<index&&index<pow2[29]);
+		int l=0,r=29;
+		while(l<r)
+		{
+			int md=(l+r)/2;
+			if(pow2[md]>index)r=md;
+			else l=md+1;
+		}
+		return r-1;
+	}
+
+
+	void change(int index,T element)
+	{
+		assert(index>=0&&index<sz);
+		
+		index++;
+		int pos=getPos(index);
+		link[pos][index-pow2[pos]]=element;
+	}
+	//extra
+	template <typename E>
+	class MyIterator : public Iterator<E>
+	{
+	private:
+		int version;
+		int index;
+		E value;
+		ArrayList<E>* root;
+	public:
+		MyIterator(int v,int i,ArrayList<E>* r)
+		{
+			version=v;
+			index=i;
+			root=r;
+			if(i==-1)
+			{
+				value=0;
+			}
+			else
+			{
+				value=root->get(i);
+			}
+		}
+		T next()
+		{
+			//version check
+			if(version!=root->getVersion())
+			{
+				throw logic_error("fff");
+			}
+
+			index++;
+			assert(0<=index&&index<root->sz);
+			value=root->get(index);
+			return value;
+		}
+		bool hasNext()
+		{
+			//version check
+			if(version!=root->getVersion())
+			{
+				throw logic_error("fff");
+			}
+			return index<root->sz-1;
+		}
+
+	};
+
+public:
+	ArrayList()
+	{
+		version=sz=0;
+		memset(link,0,sizeof(link));
+		pow2[0]=1;
+		for(int i=1;i<30;i++)
+			pow2[i]=pow2[i-1]*2;
+	}
+
+	int size() const
+	{
+		return sz;
+	}
+
+	bool isEmpty() const
+	{
+		return sz==0;
+	}
+
+	T get(int index) const
+	{
+		assert(index>=0&&index<sz);
+		
+		index++;
+		int pos=getPos(index);
+		return link[pos][index-pow2[pos]];
+	}
+
+	void add(T element)
+	{
+		sz++;
+		int pos=getPos(sz);
+		if(sz==pow2[pos]&&link[pos]==NULL)
+		{
+			link[pos]=new int[pow2[pos]];
+		}
+		version++;
+		link[pos][sz-pow2[pos]]=element;
+	}
+
+	T remove(int index)
+	{
+		assert(index>=0&&index<sz);
+
+		index++;
+		int pos=getPos(index);
+		T res=link[pos][index-pow2[pos]];
+		
+		for(int i=index;i<sz;i++)
+		{
+			change(i-1,get(i));
+		}
+
+		pos=getPos(sz);
+		if(sz==pow2[pos]&&link[pos+1]!=NULL)
+		{
+			delete [] link[pos+1];
+			link[pos+1]=NULL;
+		}
+		sz--;
+		version++;
+		return res;
+	}
+	//extra
+	int getVersion()
+	{
+		return version;
+	}
+
+	Iterator<T>* iterator()
+	{
+		return new MyIterator<T>(version,-1,this);
+		
+	}
+
 };
+
