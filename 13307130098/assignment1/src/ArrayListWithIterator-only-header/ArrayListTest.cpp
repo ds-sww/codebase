1273c66035d7865c2e6db725571df2d3704983e0
tarmo <tarmo@tarmo.org>
2014-10-02 19:57:46 +0800

    init

--- 13307130098/assignment1/src/ArrayListWithIterator-only-header/ArrayListTest.cpp
+++ 13307130098/assignment1/src/ArrayListWithIterator-only-header/ArrayListTest.cpp
@@ -79,14 +79,14 @@ void random_insert_delete_entrys(List<int>& list, double insertFactor) {
     int numOfOp = 100000;
 
     for (int i = 0; i < numOfOp; ++i) {
-        int op = rand() % 10; 
+        int op = random() % 10; 
         if (op <= insertFactor) {
             int num = rand() % 1000000;
             list.add(num);            
             vec.push_back(num);
         } else {
             if (vec.size() > 0) {
-                int num = rand() % vec.size();
+                int num = random() % vec.size();
 
                 erase(vec, num);
                 list.remove(num);
