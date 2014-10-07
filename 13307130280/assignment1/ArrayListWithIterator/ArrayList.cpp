34eaf8f4070defa836a7ad0c818b4d4ad62aea6b
=qini7 <=>
2014-10-07 20:54:17 +0800

    13307130280_assignment1

--- 13307130280/assignment1/ArrayListWithIterator/ArrayList.cpp
+++ 13307130280/assignment1/ArrayListWithIterator/ArrayList.cpp
@@ -1,181 +1,130 @@
 
 /**
- * @author whimsycwd
- * @date   2014.9.26
+ * @author qini7
+ * @date   2014.10.07
  * 类似Vector
  */
-#include <iostream>
-#include <cassert>
-#include "List.h"
-
+#include<iostream>
+#include<cassert>
+#include<stdexcept>
+#include"List.h"
 using namespace std;
 
 template <typename T>
-class ArrayList : public List<T> {
+class ArrayList:public List<T>
+{
 
-    private :
-        T* array;
-        int cnt;
-        int capacity;
+	template <typename E>
+	class ArrayIterator:public Iterator<E>
+	{
+	public:
+		ArrayIterator(ArrayList<E>* outer)
+		{
+			it_size=0;
+			it_time=outer->list_time;
+			this->outer=outer;
+		}
+		~ArrayIterator()
+		{
+			outer=0;
+		}
+		bool hasNext()
+		{
+			TimeCheck();
+			return it_size < outer->list_size;
+		}
+		E next()
+		{
+			TimeCheck();
+			if (it_size >= outer->list_size)
+				throw out_of_range("ArrayIterator");
+			return outer->get(it_size++);
+		}
+	private:
+		int it_size,it_time;
+		ArrayList<E>* outer;
+		void TimeCheck()
+		{
+			if (it_time != outer->list_time)
+				throw logic_error("the host arrayList has been changed!");
+		}
+	};
+public:
+	ArrayList()
+	{
+		array=new T;
+		list_size=0;
+		list_msize=1;
+		list_time=0;
+	}
+	~ArrayList()
+	{
+		delete[] array;
+	}
+	int size() const
+	{	
+		return list_size;
+	}
+	bool isEmpty() const
+	{
+		return (!list_size);
+	}
+	T get(int index) const
+	{
+		assert(index>=0 && index<list_size);
+		return array[index]; 
+	}
+	void add(const T& element)
+	{
+		list_time++;
+		if (list_msize>list_size)
+		{
+			array[list_size++]=element;
+		}
+		else
+		{
+			T *tarray;
+			tarray=array;
+			list_msize+=list_msize;
+			array=new T[list_msize];
+			for (int i=0;i<list_size;i++)
+				array[i]=tarray[i];
+			delete[] tarray;
+			array[list_size++]=element;
+		}
+	}
+	T remove(int index)
+	{
 
-        int modCnt;  // modification counter
-
-        const static int initialCapacity = 16;
-
-        void arrayCopy(T* dest, T* sou, int num) {
-            for (int i = 0; i< num; ++i) {
-                dest[i] = sou[i];
-            }
-        }
-
-        void expandCapacity() {
-            capacity = capacity * 2;
-            T * newArray = new T[capacity];    
-            
-            arrayCopy(newArray, array, cnt);
-
-            delete [] array;
-            array = newArray;
-        }
-        void shrinkCapacity() {
-            // funny thing happen without this line
-            if (capacity <= initialCapacity) {
-                return;
-            }
-
-            capacity = capacity / 2;
-            T * newArray = new T[capacity];
-            
-            arrayCopy(newArray, array, cnt);
-
-            delete [] array;
-            array = newArray;
-
-        }
-
-        void rangeCheck(int index) const {
-            assert(index >= 0 && index < cnt);
-        }
-       
-        /**
-         * Iterator Implementation
-         */
-
-        template <typename E>
-        class ArrayIterator : public Iterator<E> {
-            private :
-                int itCnt;
-                int itModCnt;
-
-                ArrayList<E>* outer;
-
-                void modifiedCheck() {
-                    if (itModCnt != outer->modCnt) {
-                        throw logic_error("the host arrayList has been changed!");
-                    }
-                }
-            
-            public :
-                ArrayIterator(ArrayList<E>* outer) {
-                    itCnt = 0;
-                    itModCnt = outer->modCnt;
-                    this->outer = outer;
-                }     
-                
-                bool hasNext() {
-                    modifiedCheck();
-                    
-                    return itCnt < outer->cnt;
-                }
-                E next() {
-                    modifiedCheck();
-                    
-                    if (itCnt >= outer->cnt) {
-                        throw out_of_range("ArrayIterator");
-                    }
-
-                    return outer->get(itCnt++);
-                }
-                virtual ~ArrayIterator() {
-                    outer = 0;
-                }
-                 
-        };
-
-    public :
-
-        ArrayList() {
-            capacity = initialCapacity;
-            array = new T[capacity];
-            cnt = 0;
-        }
-
-        ArrayList(int defaultCapacity) {
-            if (defaultCapacity <= initialCapacity) {
-                defaultCapacity = initialCapacity;
-            }
-            capacity = defaultCapacity;
-            array = new T[capacity];
-            cnt = 0;
-            
-        }
-
-        int size() const {
-            return cnt;
-        }
-        bool isEmpty() const {
-            return cnt == 0;
-        }
-        T get(int index) const {
-            rangeCheck(index);
-        
-            return array[index];
-        }
-        void add(const T& element) {
-            if (cnt == capacity) {
-                expandCapacity();
-            }
-            
-            // for fast fail 
-            ++modCnt;
-
-            array[cnt++] = element; 
-        } 
-        
-        T remove(int index) {
-            rangeCheck(index);
-            
-            T retValue = array[index];
-
-            for (int i = index; i < cnt-1; ++i) {
-                array[i] = array[i + 1];
-            }
-            
-            // for fast fail
-            ++modCnt;
-
-            // TODO: suitable? 
-            // even if array store pointer, we can't free it in this container. 
-            // or it will cause dangle pointer outside.
-            // 0 is amazing~ stand for numeric and NULL!
-            array[--cnt] = 0;
-
-            if (cnt <= capacity / 4) {
-                shrinkCapacity();
-            }
-            return retValue;
-        }
-
-        // tricky, return a pointer, how to ensure this ArrayIterator free properly
-        // can we manage this allocated Iterators by self?
+		T temp;
+		assert(index>=0 && index<list_size);
+		temp=array[index];
+		list_time++;
+		if (list_size<=(list_msize>>2))
+		{	
+			list_msize>>1;
+			T *tarray;
+			tarray=array;
+			array=new T[list_msize];
+			for (int i=0;i<list_size;i++)
+			{
+				if (i<index) array[i]=tarray[i];
+				else array[i]=tarray[i+1];
+			}
+			delete[] tarray;
+		}
+		else
+		{
+			for (int i=index;i<list_size-1;i++)
+				array[i]=array[i+1];
+		}
+		array[--list_size]=0;
+		return temp;
+	}
         Iterator<T>* iterator() {
-            return new ArrayIterator<int>(this);
+            return new ArrayIterator<T>(this);
         }
-
-        ~ArrayList() {
-            delete [] array;
-        }
-
+private:
+	T *array;
+	int list_size,list_msize,list_time;
 };
-
-
