412df7d09d7213e956f0afda87ba06ef38b0fbc8
Orthocenter <chenyuxin.mail@gmail.com>
2014-10-08 20:51:20 +0800

    Creat ArrayListWithIterator/.

---
+++ 13307130248/assignment1/ArrayListWithIterator-only-header/ArrayList.cpp
@@ -0,0 +1,95 @@
+
+/**
+ * @author whimsycwd
+ * @date   2014.9.26
+ * 类似Vector
+ */
+#include <iostream>
+#include <cassert>
+#include "List.h"
+
+using namespace std;
+
+template <typename T>
+class ArrayList : public List<T> {
+
+private:
+
+	T *array;
+	int mCapacity, mSize;
+
+	void resize(int n, T val = T())
+	{
+		T *old_array = array;
+		
+		mCapacity = n;
+
+		array = new T[mCapacity]();
+
+		for(int i = 0; i < mSize; i++)
+		{
+			array[i] = old_array[i];
+		}
+
+		delete[] old_array;
+	}
+
+public:
+
+	ArrayList()
+	{
+		mSize = 0;
+		mCapacity = MINCAPACITY;
+		array = new T[mCapacity]();
+	}
+
+	~ArrayList()
+	{
+		delete[] array;
+	}
+
+	int size() const
+	{
+		return mSize;
+	}
+
+	bool isEmpty() const
+	{
+		return mSize == 0;
+	}
+
+	T get(int index) const
+	{
+		return array[index];
+	}
+
+	void add(T& element)
+	{
+		// capacity is not enough
+		if(mSize == mCapacity)
+		{
+			resize(mCapacity << 1);
+		}
+
+		array[mSize++] = element;
+	}
+
+	T remove(int index)
+	{
+		int element = array[index];
+		
+		mSize--;
+		
+		for(int i = index; i < mSize; i++)
+		{
+			array[i] = array[i + 1];
+		}
+
+		if(mCapacity > MINCAPACITY && double(mSize) / mCapacity < 0.20)
+		{
+			resize(max(MINCAPACITY, mSize << 1));
+		}
+
+		return element;
+	}
+};
---
+++ 13307130248/assignment1/ArrayListWithIterator-only-header/Iterator.h
@@ -0,0 +1,22 @@
+/**
+ * @author ChenYuxin
+ * @date   2014.10.8
+ * 迭代器类， 通过该类，可以遍历容器
+ */
+#ifndef _ITERATOR_H
+#define _ITERATOR_H 1
+
+template <typename T>
+class Iterator {
+    public :
+        /**
+        * Return true if the iteration has more elements
+        */
+        virtual bool hasNext() = 0;
+        /**
+        *  Returns the next element in the iteration
+        */
+        virtual T next() = 0;
+};
+
+#endif
