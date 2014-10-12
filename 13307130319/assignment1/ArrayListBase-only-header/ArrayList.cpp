dc2499de77a31bae12fd8fb31eda527662f7fa1a
Xiaotao Liang <418xiaotao@163.com>
2014-10-12 09:56:29 +0800

    finish assignment1

--- 13307130319/assignment1/ArrayListBase-only-header/ArrayList.cpp
+++ 13307130319/assignment1/ArrayListBase-only-header/ArrayList.cpp
@@ -1,9 +1,3 @@
-
-/**
- * @author whimsycwd
- * @date   2014.9.26
- * 类似Vector
- */
 #include <iostream>
 #include <cassert>
 #include "List.h"
@@ -11,5 +5,71 @@
 using namespace std;
 
 template <typename T>
-class ArrayList : public List<T> {
+class ArrayList: public List<T>
+{
+	private:
+
+	const static int sizeDefault=1024;
+	int n, Maxn;
+	T *arr;
+
+	public:
+
+	ArrayList()
+	{
+		Maxn=sizeDefault;
+		n=0;
+		arr=new int[Maxn];
+	}
+
+	~ArrayList()
+	{
+		delete[] arr;
+	}
+
+	int size() const
+	{
+		return n;
+	}
+
+	bool isEmpty() const
+	{
+		return n==0;
+	}
+
+	T get(int index) const
+	{
+		assert(0<=index && index<n);
+		return arr[index];
+	}
+
+	void add(const T &element)
+	{
+		if (n==Maxn)
+		{
+			Maxn<<=1;
+			T *temp=new T[Maxn];
+			for (int i=0; i<n; ++i) temp[i]=arr[i];
+			delete[] arr;
+			arr=temp;
+		}
+		arr[n++]=element;
+	}
+
+	T remove(int index)
+	{
+		T element=get(index);
+		for (int i=index+1; i<n; ++i) arr[i-1]=arr[i];
+		--n;
+		if (n*4<=Maxn && Maxn/2>=sizeDefault)
+		{
+			Maxn>>=1;
+			T *temp=new T[Maxn];
+			for (int i=0; i<n; ++i) temp[i]=arr[i];
+			delete[] arr;
+			arr=temp;
+		}
+		return element;
+	}
 };
+
