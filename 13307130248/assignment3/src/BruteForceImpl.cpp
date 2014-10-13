3557e25b88789bccf7b09c1efde11131b85c8fad
Orthocenter <chenyuxin.mail@gmail.com>
2014-10-13 19:48:37 +0800

    Finish Brute Force.

--- 13307130248/assignment3/src/BruteForceImpl.cpp
+++ 13307130248/assignment3/src/BruteForceImpl.cpp
@@ -19,10 +19,34 @@ class BruteForceImpl : public Matcher {
 
 
     public :
-        explicit BruteForceImpl(const string& pattern) {
+        explicit BruteForceImpl(const string& _pattern) {
+        	pattern = _pattern;
         }
 
         virtual int find(string text) {
+        	int len_pattern = pattern.length();
+        	int len_text = text.length();
+
+        	int find_end = len_text - len_pattern + 1;
+
+			for(int i = 0; i < find_end; i++)
+			{
+				bool equal = true;
+				for(int j = 0; j < len_pattern; j++)
+				{
+					if(pattern[j] != text[i + j])
+					{
+						equal = false;
+						break;
+					}
+				}
+				
+				if(equal)
+				{
+					return i;
+				}
+			}
+
             return NOT_FOUND;
         }
         
