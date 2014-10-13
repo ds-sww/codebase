82882a9014699efece59cbd165738d7b413c2317
billlai95 <billlai@foxmail.com>
2014-10-13 16:30:07 +0800

    My solution, which seems a little naive.

--- 13307130346/assignment2/merge_fruit.cpp
+++ 13307130346/assignment2/merge_fruit.cpp
@@ -1,61 +1,94 @@
-/**
- * Author   : Ryannnnnnn
- * Date     : Sep 30, 2014
- */
+//
+//  main.cpp
+//  combinenuts
+//
+//  Created by billlai on 14-9-29.
+//  Copyright (c) 2014年 billlai. All rights reserved.
+//
 
-#include <cstdio>
-#include <vector>
-#include <queue>
-#include <algorithm>
-
+#include <iostream>
 using namespace std;
-
-
-class MergeFruit {
-private:
-    int popMinimumItem(queue <int> &a, queue <int> &b) {
-        int item;
-        if (b.empty() || (!a.empty() && a.front() <= b.front())) {
-            item = a.front(); a.pop();
-        } else {
-            item = b.front(); b.pop();
-        }
-        return item;
-    }
-
+class cn
+{
 public:
-    int minCost(vector <int> weights) {
-        sort(weights.begin(), weights.end());
-
-        queue <int> unmeged;
-        queue <int> merged;
-        for (vector <int>::iterator it = weights.begin(); it != weights.end(); it++)
-           unmeged.push(*it);
-        int cost = 0;
-
-        while (unmeged.size() + merged.size() > 1) {
-            int item1 = popMinimumItem(unmeged, merged);
-            int item2 = popMinimumItem(unmeged, merged);
-            merged.push(item1 + item2);
-            cost += item1 + item2;
+    int kind;
+    int* nuts;
+    int ene;
+    int light1;
+    int light2;
+    int flag;
+    void input()
+    {
+        cin >> kind;
+        nuts = new int[kind];
+        for(int i = 0;i<kind;i++)
+        {
+            cin >> nuts[i];
         }
-
-        return cost;
+        flag = 0;
+        ene = 0;
     }
+    void combine()
+    {
+        light2 = 0;
+        light1 = 0;
+        for(int i = 0;i<kind;i++)
+        {
+            if(nuts[i] != -1)
+            {
+                light1 = i;
+                break;
+            }
+        }
+        for(int i = 1;i<kind;i++)
+        {
+
+            if(nuts[i] != -1 && nuts[light1]>nuts[i])
+                light1 =i;
+        }
+        for(int i = 0;i<kind;i++)
+        {
+            if(nuts[i] != -1 && light1 != i)
+            {
+                light2 = i;
+                break;
+            }
+        }
+        for(int i =0;i<kind;i++)
+        {
+            if(i == light1)
+                continue;
+            if(nuts[i] != -1 && nuts[light2]>nuts[i])
+                light2 = i;
+        }
+        if(light2 == 0 && nuts[light2]==-1 && light1 != 0)
+            flag = 1;
+        //if(light1 == 0 && light2 == 1 && nuts[light2] == -1)
+        if(light2 == 0 && light1 == 0)
+            flag = 1;
+    }
+    void startc()
+    {
+        while(flag == 0)
+        {
+            combine();
+            if(flag == 0)
+            {
+                nuts[light1]+=nuts[light2];
+                ene+=nuts[light1];
+                nuts[light2] = -1;
+            }
+        }
+        cout << ene;
+    }
+    
 };
 
-int main() {
-    int n, weight;
-    vector <int> weights;
-
-    scanf("%d", &n);
-    for (int i = 0; i < n; i++) {
-        scanf("%d", &weight);
-        weights.push_back(weight);
-    }
-
-    MergeFruit *mf = new MergeFruit();
-    printf("%d\n", mf->minCost(weights));
-
+int main()
+{
+    cn doit;
+    doit.input();
+    doit.startc();
+    
     return 0;
 }
