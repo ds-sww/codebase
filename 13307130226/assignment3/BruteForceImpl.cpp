a17182b52f621ee728a0585843c21d976d04783c
qzane <qzane@live.com>
2014-10-13 20:39:56 +0800

    BF and KR

--- 13307130226/assignment3/BruteForceImpl.cpp
+++ 13307130226/assignment3/BruteForceImpl.cpp
@@ -20,9 +20,25 @@ class BruteForceImpl : public Matcher {
 
     public :
         explicit BruteForceImpl(const string& pattern) {
+			(*this).pattern = pattern;
         }
 
         virtual int find(string text) {
+			bool find = false;
+			for(int pos=0;pos < text.size();pos++){
+				if(text.size() - pos < pattern.size())
+					return -1;
+				for(int now=0;now < pattern.size();now++)
+					if(text[now+pos] != pattern[now])
+						break;
+					else if(now == pattern.size()-1){
+						find = true;
+						break;					
+					}
+			if(find)
+				return pos;
+			}
+		
             return NOT_FOUND;
         }
         
