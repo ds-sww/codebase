172ca2929b6288cf2db997e19d375345a5c57e14
chiya <chiyahoho@gmail.com>
2014-10-15 14:41:07 +0800

    "BF & KR & KMP"

--- 13307130207/assignment3/BruteForceImpl.cpp
+++ 13307130207/assignment3/BruteForceImpl.cpp
@@ -1,13 +1,10 @@
 /**
- * @author whimsycwd
- * @date   2014.10.1
- *
+ * @author chiyahoho
+ * @date   2014.10.13
  */
 
 #include <iostream>
-#include <cstdio>
-#include <cstring>
-
+#include <string>
 #include "Matcher.h"
 
 using namespace std;
@@ -17,17 +14,26 @@ class BruteForceImpl : public Matcher {
     private :
         string pattern;
 
-
     public :
         explicit BruteForceImpl(const string& pattern) {
+            this->pattern=pattern;
         }
 
         virtual int find(string text) {
+            int n=text.length(),m=pattern.length();
+            for (int i=0;i<=n-m;i++){
+                int flag=1;
+                for(int j=0;j<m;j++)
+                    if (pattern[j]!=text[i+j]){
+                        flag=0;
+                        break;
+                    }
+                if (flag) return i;
+            }
             return NOT_FOUND;
         }
-        
+
         virtual ~BruteForceImpl() {
-            
         }
 };
 
