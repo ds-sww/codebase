03f656c1445044736df2a802d8271bfb883360eb
novinsomnia <13307130414@fudan.edu.cn>
2014-10-16 11:30:20 +0800

    assignment3[solution]

--- 13307130414/assignment3/KMPImpl.cpp
+++ 13307130414/assignment3/KMPImpl.cpp
@@ -12,15 +12,41 @@
 using namespace std;
 
 class KMPImpl : public Matcher {
-    private : 
+    private :
         string pattern;
+        const int SIZE = 100;
 
     public :
         KMPImpl(string pattern) {
+            this -> pattern = pattern;
+        }
+
+        void preprocessing(string p, int m, int Next[]) {
+            for (int i = 0, j = Next[0] = -1; i < (m - 1); ) {
+                for (; j > -1 && p[i] != p[j]; )
+                    j = Next[j];
+                i++;
+                j++;
+                if (p[i] == p[j]) Next[i] = Next[j];
+                else Next[i] = j;
+            }
         }
 
         virtual int find(string text) {
-            return NOT_FOUND; 
+            if (text.size() < pattern.size())
+                return NOT_FOUND;
+
+            int i,j,Next[SIZE];
+            preprocessing(pattern, pattern.size(), Next);
+            for (i = j = 0; j < text.size(); ) {
+                for (; i > -1 && pattern[i] != text[j]; )
+                    i = Next[i];
+                i++;
+                j++;
+                if (i >= pattern.size())
+                    return (j - i);
+            }
+            return NOT_FOUND;
         }
 
         virtual ~KMPImpl() {
