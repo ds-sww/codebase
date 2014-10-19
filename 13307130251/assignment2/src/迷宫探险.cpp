341a7522bcc7a647000e66fb9b7a897d89afa6b5
Eric huang <q465168867@gmail.com>
2014-10-19 22:48:39 +0800

    complete the basic requirements

---
+++ 13307130251/assignment2/src/迷宫探险.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int N,M;
+cin>>N>>M;
+char **maze=new char[N][M];
+for(int i=0;i<N;i++)
+	for(int j=0;j<M;j++)
+		cin>>char[i][j];
+}
+int Find(char **maze){
+
