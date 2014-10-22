e6dac7a3e9fdbe7dd46abfd09e6463761b501ef6
Huiying Li <13307130498@fudan.edu.cn>
2014-10-22 22:00:10 +0800

    BF KR&KMP completed

--- 13307130498/assignment3/KMPImpl.cpp
+++ 13307130498/assignment3/KMPImpl.cpp
@@ -14,15 +14,42 @@ using namespace std;
 class KMPImpl : public Matcher {
     private : 
         string pattern;
-
+        int len;
+        int *next;
     public :
         KMPImpl(string pattern) {
+        	this->pattern = pattern;
+        	len = pattern.length();
+        	next = new int [len];
+        	int i,j;
+        	i=0;
+        	next[i] = -1;
+        	next[i+1] = 0;
+        	for(i=1;i<len;i++){
+        		j=i;
+        		while(j>=1){
+        			j = next[j];
+        			if(pattern[j]==pattern[i]){next[i+1] = j+1;break;}	
+        		}
+        		next[i+1] = 0;
         }
 
         virtual int find(string text) {
+        	int length = text.length();
+        	int i,j;
+        	for(i=0;i+len<=length;){
+        		for(j=0;j<len;j++){
+        			if(pattern[j]!=text[i+j])break;
+        		}
+        		if(j==len)break;
+        		i = j-next[j];
+        	}
+        	if(j==len)return i;
             return NOT_FOUND; 
         }
 
         virtual ~KMPImpl() {
         }
+        
+        
 };
