079c1d353fc67e36eca20a1e81c0a2a6e667e557
qzane <qzane@live.com>
2014-10-29 13:56:37 +0800

    明白了，括号的问题是中间过程出现负数导致的，已解决，第三个点仍然不过仍然想不通

--- 13307130226/assignment5/p2.cpp
+++ 13307130226/assignment5/p2.cpp
@@ -6,7 +6,7 @@ using namespace std;
 string pattern;
 
 bool check(const string& a){
-	if(((a[0] <'0' or a[0] > '9') and a[0] != '-'))
+	if(((a[0] <'0' or a[0] > '9') and a[0] != '-' and a[0] != '@'))
 		return false;
 	for(int i=1;i<a.length();i++)
 		if(((a[i] <'0' or a[i] > '9')))
@@ -31,10 +31,13 @@ int find_left_digit(const string& a,int i){
 		--i;
 	if(i == 0)
 	return 0;
-	else
+	else if(a[i]=='@')
+		return i;
 	return i+1;
 }
 int find_right_digit(const string& a,int i){
+	if(a[i]=='@')
+		++i;
 	while(a[i] >= '0' and a[i] <= '9' and i<a.length())
 		++i;
 	if(i == a.length())
@@ -44,7 +47,7 @@ int find_right_digit(const string& a,int i){
 }
 int to_digit(const string& a,int i){
 	int tmp;bool ne;
-	if(a[i]=='-'){
+	if(a[i]=='@'){
 		ne = true;
 		i++;
 	}
@@ -70,7 +73,7 @@ string to_string(int ans){
 	for(;ans>0;ans /=10)
 		tmp=(char)(ans%10+'0')+tmp;
 	if(ne)
-		tmp = '-' + tmp;
+		tmp = '@' + tmp;
 	return tmp;
 }
 
@@ -97,13 +100,6 @@ string power(const string& a,const string& b){
 	for(int i=0;i<w;i++)
 		ans*=q;
 	return to_string(ans);
-	/*
-	tmp = "";
-	for(;ans>0;ans /=10)
-		tmp+=(char)(ans%10+'0');
-	reverse(tmp);	
-	return tmp;
-	*/
 }
 string multiply(const string& a,const string& b){
 	string tmp;
@@ -111,12 +107,6 @@ string multiply(const string& a,const string& b){
 	tmp = "";
 	ans = to_digit(a,0)*to_digit(b,0);
 	return to_string(ans);
-	/*
-	for(;ans>0;ans /=10)
-		tmp+=(char)(ans%10+'0');
-	reverse(tmp);	
-	return tmp;	
-	*/
 
 }
 string divid(const string& a,const string& b){
@@ -125,13 +115,6 @@ string divid(const string& a,const string& b){
 	tmp = "";
 	ans = to_digit(a,0)/to_digit(b,0);
 	return to_string(ans);
-	/*
-	cout << "divid ans" << ans << endl;
-	for(;ans>0;ans /=10)
-		tmp+=(char)(ans%10+'0');
-	cout << "divid tmp" << tmp << endl;
-	reverse(tmp);	
-	return tmp;	*/
 }
 string solve(string a){
 	int i,l,r,ans;
@@ -145,76 +128,55 @@ string solve(string a){
 			i = l;
 		}else if(i == a.length())
 			break;
-			
-	//cout << " ^ begin" << a  << "end " << endl;
 	for(i=0;i<a.length();i++)
 		if(a[i] == '*' or a[i] =='/'){
 			l = find_left_digit(a,i-1);
 			r = find_right_digit(a,i+1);
 			if(a[i] == '*'){
+				
 				a.replace(l,r-l+1,multiply(a.substr(l,i-l),a.substr(i+1,r-i)));}
 			else if(a[i] == '/'){
-				
 				a.replace(l,r-l+1,divid(a.substr(l,i-l),a.substr(i+1,r-i)));	}		
 			i = l;
 		}else if(i == a.length())
-			break;
-	
+			break;	
 	ans = to_digit(a,0);
 	for(i=find_right_digit(a,0)+1;i<a.length();i++){
 		if(a[i]=='+'){
 			ans+=to_digit(a,i+1);
 			i = find_right_digit(a,i+1);
-		}else if(a[i]=='-'){
+		}else if(a[i]=='-' or a[i]=='@'){
 			ans-=to_digit(a,i+1);
 			i = find_right_digit(a,i+1);
 		}
 	}
-	
-	//cout << "ans  " << ans << endl;
-	//cout << "solvebegin" << to_string(ans) << endl;
 	return to_string(ans);
-	/*
-	tmp = "";
-	if(ans < 0){
-		ne = true;
-		ans = -ans;
-	}else 
-		ne = false;
-	for(;ans>0;ans /=10)
-		tmp=(char)(ans%10+'0')+tmp;
-	if(ne)
-		tmp = '-' + tmp;
-	return tmp;	*/
 }
 
 string make(string a){
 	int tmp;
-	//cout << "make" << a << endl;
 	for(int i=0;i<a.length();i++){
-		//cout << "iii" << i << " " << a[i] << endl;
 		if(a[i]=='('){
 			tmp = find_next(a,i);
-			//cout << "tmp" << i <<' ' << tmp << endl;
 			a.replace(i,tmp-i,make(a.substr(i+1,tmp-i-2)));
 		}
 		if(i==a.length())
 			break;
 	}
-	//cout << "now a" << a << endl;
-	//cout << " ans" << solve(a) << endl;
 	return solve(a);
-	
-	
-	
-	
 }
-
+void rep(string& a){
+	for(int i=0;i<a.length();i++)
+		if(a[i] == '@')
+			a[i] = '-';
+}
 int main(){
 	while(cin >> pattern){
 		while(!check(pattern))
 			pattern=make(pattern);
+		rep(pattern);
 		cout << pattern << endl;
 	}
+	return 0;
 
 }
