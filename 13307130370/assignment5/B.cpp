6d117835d58b655c136545b8d4827286acfa4a27
Sizhe Li <13307130370@fudan.edu.cn>
2014-10-29 21:25:32 +0800

    13307130370_lisizhe_assignment5

--- 13307130370/assignment5/B.cpp
+++ 13307130370/assignment5/B.cpp
@@ -1,5 +1,6 @@
 /*修改利用了了一下书上的栈定义以及中缀转后缀函数和后缀计算，
-但是过不了OJ，用一些正确的测试数据实验后也是正确的，不知道是哪里除了问题*/
+将表达式转成后缀之后进行计算，但是过不了OJ，用一些正确的测试数据实验后也是正确的，
+不知道是哪里除了问题，希望助教予以改正，谢谢～*/
 
 #include<iostream>
 #include<string>
@@ -39,7 +40,7 @@ public:
 			return 0;
 		px=stk[top--];
 		return px;
-		
+
 	}
 	Type getTop()
 	{
@@ -50,14 +51,9 @@ public:
 	int isEmpty()const{return top==-1;}
 	int isFull()const{return top==MAXN-1;}
 };
-
-
 int evaluate(int p_y, char * pos_e)
 {
-	int i,j,k,z,x,y;
-	
-	
-
+	int i,j,k,z,x,y,l=0;
 	Stack<int> s;
 	char c;
 	i=0;
@@ -65,47 +61,65 @@ int evaluate(int p_y, char * pos_e)
 	while(c!='\0')
 	{
 		if(c>='0'&&c<='9')
-			s.push(c-'0');
-		else switch(c)
 		{
-		case '+':
-			x=s.pop();
-			y=s.pop();
-			s.push((x)+(y));
-			break;
-		case '-':
-			x=s.pop();
-			y=s.pop();
-			s.push((y)-(x));
-			break;
-		case '*':
-			x=s.pop();
-			y=s.pop();
-			s.push((x)*(y));
-			break;
-		case '/':
-			x=s.pop();
-			y=s.pop();
-			s.push((y)/(x));
-			break;
-		case '^':
-			x=s.pop();
-			y=s.pop();
-			if(x==0)
-				s.push(1);
-			else
+			while(c>='0'&&c<='9')
 			{
-				if(x>0)
-					j=x;
-				else
-					j=-(x);
-				for(z=1,k=1;k<=j;k++)
-					z*=(y);
-				if(x<0)
-					z=1/z;
-				s.push(z);
+				l=10*l+(c-'0');
+				c=pos_e[++i];
 			}
-			break;
+			s.push(l);
+			l=0;
+			i--;
+		}
+		else
+		{
+			switch(c)
+			{
+			case 'p':break;
+			case '+':
+				x=s.pop();
+				y=s.pop();
+				s.push((x)+(y));
+				break;
+			case '-':
+				x=s.pop();
+				y=s.pop();
+				s.push((y)-(x));
+				break;
+			case '*':
+				x=s.pop();
+				y=s.pop();
+				s.push((x)*(y));
+				break;
+			case '/':
+				x=s.pop();
+				y=s.pop();
+				s.push((y)/(x));
+				break;
+			case '^':
+				x=s.pop();
+				y=s.pop();
+				if(y==0)
+					s.push(0);
+				else
+				{
+					if(x==0)
+						s.push(1);
+					else
+					{
+						if(x>0)
+							j=x;
+						else
+							j=-(x);
+						for(z=1,k=1;k<=j;k++)
+							z*=(y);
+						if(x<0)
+							z=1/z;
+						s.push(z);
+					}
+					break;
+				}
+			}
 		}
 		c=pos_e[++i];
 	}
@@ -113,7 +127,6 @@ int evaluate(int p_y, char * pos_e)
 	if(s.isEmpty())
 		return p_y;
 }
-
 int icp(char c)
 {
 	switch(c)
@@ -140,7 +153,7 @@ int isp(char c)
 }
 int mid_to_pos(char mid_e[],char pos_e[])
 {
-	
+
 	Stack<char>s;
 	char c;
 	int i,j;
@@ -149,32 +162,36 @@ int mid_to_pos(char mid_e[],char pos_e[])
 	while(c!='\0')
 	{
 		if(isdigit(c))
-			pos_e[j++]=c;
-		else switch(c)
 		{
-		case'+':
-		case'-':
-		case'*':
-		case'/':
-		case'^':
-			while(icp(c)<=isp(s.getTop()))
+			pos_e[j++]=c;
+		}
+		else 
+		{
+			pos_e[j++]='p';  //用p来分割不同的数字
+			switch(c)
 			{
-				pos_e[j++]=s.pop();
+			case'+':
+			case'-':
+			case'*':
+			case'/':
+			case'^':
+				while(icp(c)<=isp(s.getTop()))
+				{
+					pos_e[j++]=s.pop();
+				}
+				s.push(c);
+				break;
+			case'(':
+				s.push(c);
+				break;
+			case')':
+				while((s.getTop())!='(')
+				{
+					pos_e[j++]=s.pop();
+				}
+				break;
+			default:return 1;
 			}
-			s.push(c);
-			break;
-		case'(':
-			s.push(c);
-			break;
-		case')':
-			while((s.getTop())!='(')
-			{
-				
-
-				pos_e[j++]=s.pop();
-			}
-			break;
-		default:return 1;
 		}
 		c=mid_e[++i];
 	}
@@ -185,7 +202,6 @@ int mid_to_pos(char mid_e[],char pos_e[])
 	pos_e[j]='\0';
 	return 0;
 }
-
 int main()
 {
 	string q[100];
@@ -195,6 +211,7 @@ int main()
 	int number =0;
 	v=0;
 	while(cin>>q[v])
+
 		v++;
 	for(int i=0;i<v;i++)
 	{
@@ -203,7 +220,6 @@ int main()
 		number=evaluate(number,pos_e);
 		cout<<number<<endl;
 	}
-	
 	return 0;
 }
 
