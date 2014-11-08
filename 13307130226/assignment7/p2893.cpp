09d35370e07eecac0769771747df9fc2a9f4d43e
qzane <qzane@live.com>
2014-11-08 17:10:10 +0800

    谁再说cin不慢的试试这个代码，我关了流同步了，scanf只要1579MS，用cin 4S都会超时

--- 13307130226/assignment7/p2893.cpp
+++ 13307130226/assignment7/p2893.cpp
@@ -17,9 +17,9 @@ void merge_sort(vector<int>::iterator l,vector<int>::iterator r){
 	mid = l+((r-l)>>1);
 	merge_sort(l,mid);
 	merge_sort(mid,r);
-	merge_tmp.resize(mid-l);
-	for(i=l,now=merge_tmp.begin();i!=mid;i++)
-		*(now++)=*i;
+	merge_tmp.clear();
+	for(i=l;i!=mid;i++)
+		merge_tmp.push_back(*i);
 	for(i=merge_tmp.begin(),j=mid,now=l;i!=merge_tmp.end() && j!=r;now++){
 		if(*i <= *j){
 			*now = *i;
@@ -39,18 +39,19 @@ void merge_sort(vector<int>::iterator l,vector<int>::iterator r){
 int main(){
 	std::ios_base::sync_with_stdio(false);
 	while(1){
-		cin >> m >> n;
+		scanf("%d%d",&m,&n);
+		//cin >> m >> n;
 		if(m == 0 && n == 0)
 			return 0;
-		map.resize(m*n-1);
-		h = 0;
+		map.clear();
 		for(i=0;i<m;i++)
 			for(j=0;j<n;j++){
-			cin >> g;
+			//cin >> g;
+			scanf("%d",&g);
 			if(g == 0)
 				h0=i;
 			else
-				map[h++]=g;
+				map.push_back(g);
 			}
 		countt = 0;
 		merge_sort(map.begin(),map.end());
@@ -60,15 +61,19 @@ int main(){
 		countt &= 1;
 		if(n==1){
 			if(countt==0)
-				cout << "YES" << endl;
+				printf("YES\n");
+				//cout << "YES" << endl;
 			else
-				cout << "NO" << endl;
+				printf("NO\n");
+				//cout << "NO" << endl;
 		}
 		if(n==0){
 			if(countt ^ h0 == 0)
-				cout << "YES" << endl;
+				printf("YES\n");
+				//cout << "YES" << endl;
 			else
-				cout << "NO" << endl;
+				printf("NO\n");
+				//cout << "NO" << endl;
 		}
 	}
 
