0891088c9c2d50e061ebf9770750d9e471b978db
Eric huang <q465168867@gmail.com>
2014-11-18 11:56:54 +0800

    finished BF by myself

--- 13307130251/assignment3/BruteForceImpl.cpp
+++ 13307130251/assignment3/BruteForceImpl.cpp
@@ -15,16 +15,22 @@ class BruteForceImpl : public Matcher {
 
     public :
         explicit BruteForceImpl(const string& pattern) {
+			this->pattern = pattern;
         }
 
         virtual int find(string text) {
+			for(int i=0;text[i]!=0;i++)
+				for(int j=0;pattern[j]!=0;j++){
+					if(text[i+j]!=pattern[j])
+						break;
+					if(pattern[j+1]==0)
+						return i;
+				}
             return NOT_FOUND;
         }
         
         virtual ~BruteForceImpl() {
-            
         }
 };
-
 
 
